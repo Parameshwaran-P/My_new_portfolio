64e1b6d3de3c7c19530a18fa1bdb1024582b667d
paramesh-75 <parameshwaranit22@gmail.com>
2024-07-21 20:16:16 +0530

    resume name updated

--- src/components/HeroContent/HeroContent.jsx
+++ src/components/HeroContent/HeroContent.jsx
@@ -4,10 +4,10 @@ import './HeroContent.css'
 
 const HeroContent = () => {
   function handleClick(){
-    const pdfUrl = "Paramesh_Resume.pdf";
+    const pdfUrl = "Parameshwaran_Profile.pdf";
         const link = document.createElement("a");
         link.href = pdfUrl;
-        link.download = "document.pdf"; // specify the filename
+        link.download = "Paramesh_Profile.pdf"; // specify the filename
         document.body.appendChild(link);
         link.click();
         document.body.removeChild(link);
